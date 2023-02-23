f44be5982be55e7ab228ef875aabb24ee19f1558
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-02-22 23:40:13 -0500

    First commit for new project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pioneer Valley Painting Services</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="../resources/css/app.css" />
+
+
+        
+    </head>
+    <body>
+    <div id="header">
+      <img class="logo" src="images/PVPSlogowhite.jpg">
+      <div class="menu">
+        <h1>Pioneer Valley Painting Services</h1>
+        <ul class="menubar">
+          <li><a href="index.html"><span>Home</span></a></li>
+          <li><a href="services.html"><span>Services</span></a></li>
+          <li class="active"><a href="about.html"><span>About Us</span></a></li>
+          <li><a href="process.html"><span>Our Process</span></a></li>
+          <li><a href="gallery.html"><span>Gallery</span></a></li>
+          <li><a href="contact.php"><span>Schedule an Estimate</span></a></li>
+        </ul>
+      </div>
+      <div class="bar">
+      <a></a> 
+      </div>
+    </div>
+    <div id="body">
+      <div class="bodywrap">
+        
+      </div>
+    </div>
+    <div id="footer">
+      <div class="footer_container">
+        <br>
+        <h1 class="footer_head">Why Do Our Customers Choose Pioneer Valley Painting Services?</h1>
+        <br><br>
+        <div class="row">
+          <div class="left">
+            <div class="header_wrapper">
+              <img class="check" src="../images/check.png">
+              <h1>Superior Quality</h1>
+            </div>
+            <br>
+            <p>Our painters are specially trained to deliver clean cuts, even coats, and smooth finishes. Our quality is unmatched.</p>
+          </div>
+          <div class="middle">
+            <div class="header_wrapper">
+              <img class="check" src="images/check.png">
+              <h1>Satisfaction Guaranteed</h1>
+            </div>
+            <br>
+            <p>Our commitment to quality service is so great that we don't leave the jobsite until you are 100% satisfied with our work.</p>
+          </div>
+          <div class="right">
+            <div class="header_wrapper">
+              <img class="check" src="images/check.png">
+              <h1>Customer Service</h1>
+            </div>
+            <br>
+            <p>All messages are responded to within one day, even on weekends! We're here for you, whenever you need us.</p>
+          </div>
+          <br><br>
+        </div>
+      </div>
+      <br>
+      <div id="footertags">
+        <p>Copyright 2022 Pioneer Valley Painting Services LLC</p>
+      </div>
+    </div>
+  </body>
+</html>
